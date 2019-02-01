f19fe1667832c9c59fc4d55c824d0ce26f0ea7a0
saboor922 <abdussaboor2002@hotmail.com>
2019-02-01 10:57:57 +0000

    Initial Commit and Spring Security Setup

---
+++ src/main/java/com/rest/security/SpringRestSecuritySample/SpringRestSecuritySampleApplication.java
@@ -0,0 +1,14 @@
+package com.rest.security.SpringRestSecuritySample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSecuritySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSecuritySampleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/rest/security/SpringRestSecuritySample/SpringRestSecuritySampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rest.security.SpringRestSecuritySample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestSecuritySampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
